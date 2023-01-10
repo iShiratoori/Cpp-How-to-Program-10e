32e7ddb1e8d894b1936b38a0e5efef72ccbfc1fd
Abdirahman Hussein <66288723+iShiratoori@users.noreply.github.com>
2023-01-10 01:02:47 +0000

    Delete Ex07_26.cpp

--- Ex07_26.cpp
+++
@@ -1,8 +0,0 @@
-/*
-**  Filename:    Ex07_26.cpp
-**  Author:      Abdirahman Hussein
-**  Email:       shiratoori2022@gmail.com
-**  Date:        15/11/2022
-
-Description of the problem   (Dice Rolling)
-*/
